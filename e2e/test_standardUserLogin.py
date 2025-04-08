6aa58a481a0bc1f418932f6e8223158fee84cb2c
g.gurgenashvili <ggurgenashvili1@gmail.com>
2025-04-08 16:24:32 +0400

    added credentials json

--- e2e/test_standardUserLogin.py
+++ e2e/test_standardUserLogin.py
@@ -1,11 +1,22 @@
+import json
+import os
+import sys
 import pytest
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pageObjects.standardUserLogin import StandardUserLogin
 
-def test_userLogin(browserInstance):
+test_data_path = '../data/test_standardUserLogin.json'
+with open(test_data_path) as f:
+    test_data = json.load(f)
+    test_list = test_data['data']
+
+@pytest.mark.smoke
+@pytest.mark.parametrize('test_list_item', test_list)
+def test_userLogin(browserInstance, test_list_item):
     driver = browserInstance
     userLogin = StandardUserLogin(driver)
     print(userLogin.getTitle())
-    userLogin.login('standard_user', 'secret_sauce')
+    userLogin.login(test_list_item['username'], test_list_item['password'],)
 
 
