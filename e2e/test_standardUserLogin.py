5d413cf485d13977be0c1f15a7de0319fae6d407
g.gurgenashvili <ggurgenashvili1@gmail.com>
2025-04-10 11:02:18 +0400

    checkout

--- e2e/test_standardUserLogin.py
+++ e2e/test_standardUserLogin.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import pytest
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pageObjects.standardUserLogin import StandardUserLogin
@@ -11,12 +12,10 @@ with open(test_data_path) as f:
     test_data = json.load(f)
     test_list = test_data['data']
 
-@pytest.mark.smoke
 @pytest.mark.parametrize('test_list_item', test_list)
 def test_userLogin(browserInstance, test_list_item):
     driver = browserInstance
     userLogin = StandardUserLogin(driver)
     print(userLogin.getTitle())
-    userLogin.login(test_list_item['username'], test_list_item['password'],)
-
-
+    userLogin.login(test_list_item['username'], test_list_item['password'])
+    userLogin.checkOut(test_list_item['firstName'], test_list_item['lastName'], test_list_item['zipCode'])
--- pageObjects/standardUserLogin.py
+++ pageObjects/standardUserLogin.py
@@ -12,6 +12,16 @@ class StandardUserLogin(BrowserUtils):
         self.loginBtn = (By.ID, 'login-button')
         self.header = (By.CLASS_NAME, 'header_label')
 
+        self.addToCart = (By.TAG_NAME, 'button')
+        self.shopCartCont = (By.ID, 'shopping_cart_container')
+        self.checkOutBtn = (By.ID, 'checkout')
+        self.firstName_input = (By.ID, 'first-name')
+        self.lastName_input = (By.ID, 'last-name')
+        self.zipCode = (By.ID, 'postal-code')
+        self.contBtn = (By.ID, 'continue')
+        self.finishBtn = (By.ID, 'finish')
+        self.backToProductsBtn = (By.ID, 'back-to-products')
+        self.products_header = (By.CLASS_NAME, 'header_secondary_container')
 
     def login(self, username, password):
         self.driver.find_element(*self.username_input).send_keys(username)
@@ -19,3 +29,17 @@ class StandardUserLogin(BrowserUtils):
         self.driver.find_element(*self.loginBtn).click()
         # verify that we logged in and header label appears
         assert 'Swag Labs' in self.driver.find_element(*self.header).text
+
+    def checkOut(self, firstName, lastName, zipCode):
+        buttons = self.driver.find_elements(*self.addToCart)
+        buttons[2].click()
+        self.driver.find_element(*self.shopCartCont).click()
+        self.driver.find_element(*self.checkOutBtn).click()
+        self.driver.find_element(*self.firstName_input).send_keys(firstName)
+        self.driver.find_element(*self.lastName_input).send_keys(lastName)
+        self.driver.find_element(*self.zipCode).send_keys(zipCode)
+        self.driver.find_element(*self.contBtn).click()
+        self.driver.find_element(*self.finishBtn).click()
+        self.driver.find_element(*self.backToProductsBtn).click()
+        #verify that we are back on products page
+        assert 'Products' in self.driver.find_element(*self.products_header).text
